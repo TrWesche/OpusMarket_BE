a8c43372eb9b484b29ddf3e998bfe71e686be273
Trenton Wesche <TrWesche@live.com>
2021-01-16 18:17:18 -0600

    Try adding domain again

--- helpers/authHandling.js
+++ helpers/authHandling.js
@@ -9,10 +9,10 @@ class AuthHandling {
         const split_token = token.split(".");
 
         // Javascript Enabled Cookie - Full JWT
-        queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, secure: true, sameSite: "None", path: '/'});
+        queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, secure: true, sameSite: "None", path: '/', domain: 'opusmarket-frontend.herokuapp.com'});
 
         // HTTP Only Cookie - JWT Signature Only
-        queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, secure: true, sameSite: "None", path: '/'});
+        queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, secure: true, sameSite: "None", path: '/', domain: 'opusmarket-frontend.herokuapp.com'});
     }
 
     static validateCookies(queryReq) {
