7304ba57e8d6a1a9dd739f4977433c17419e065e
Trenton Wesche <TrWesche@live.com>
2021-01-17 16:07:24 -0600

    add domain

--- helpers/authHandling.js
+++ helpers/authHandling.js
@@ -10,11 +10,11 @@ class AuthHandling {
 
         // Javascript Enabled Cookie - Full JWT
         // queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, secure: true, sameSite: "None", path: '/'});
-        queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, path: '/'});
+        queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, path: '/', domain: "trwesche.com"});
 
         // HTTP Only Cookie - JWT Signature Only
         // queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, secure: true, sameSite: "None", path: '/'});
-        queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, path: '/'});
+        queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, path: '/', domain: "trwesche.com"});
     }
 
     static validateCookies(queryReq) {
