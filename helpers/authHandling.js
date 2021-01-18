10f40084a7cb380797ba14159e3e95a27fb041d6
Trenton Wesche <TrWesche@live.com>
2021-01-17 18:05:20 -0600

    try reenabling secure cookies

--- helpers/authHandling.js
+++ helpers/authHandling.js
@@ -9,12 +9,12 @@ class AuthHandling {
         const split_token = token.split(".");
 
         // Javascript Enabled Cookie - Full JWT
-        // queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, secure: true, sameSite: "None", path: '/'});
-        queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, path: '/', domain: "twesche.com"});
+        queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, secure: true, sameSite: "None", path: '/', domain: "twesche.com"});
+        // queryRes.cookie("sid", token, {httpOnly: false, maxAge: 86400000, path: '/', domain: "twesche.com"});
 
         // HTTP Only Cookie - JWT Signature Only
-        // queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, secure: true, sameSite: "None", path: '/'});
-        queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, path: '/', domain: "twesche.com"});
+        queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, secure: true, sameSite: "None", path: '/', domain: "twesche.com"});
+        // queryRes.cookie("_sid", split_token[2], {httpOnly: true, maxAge: 86400000, path: '/', domain: "twesche.com"});
     }
 
     static validateCookies(queryReq) {
